fa5f3a478d0ca080dea885d5bdc4d2e07dbc71c6
muzammil7455-cpu <muzammil7455@gmail.com>
2025-10-31 09:33:29 -0700

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,5 @@
+alert("Welcome to my website!");
+alert("Error! Please enter a valid password.");
+alert("Welcome to JS Land...\nHappy Coding!");
+alert("Welcome to JS Land...");
+alert("Happy Coding!");
